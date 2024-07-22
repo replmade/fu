5404e36546a835866e07b81453822a0a5939ec62
ochsec <chris.ochsenreither@tsgs.com>
2024-07-21 22:06:45 -0700

    updated signin cmd

--- cmd/signin.go
+++ cmd/signin.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"fmt"
 	"fu/global"
+	"fu/internal"
 	"github.com/spf13/cobra"
+	fb "github.com/replmade/firebase-spells-go/auth"
 	"log"
 )
 
@@ -14,18 +16,53 @@ var (
 
 var signinCmd = &cobra.Command{
 	Use:   "signin",
-	Short: "Sign in a Firebase user with email and password and retrieve and id token",
+	Short: "Sign in a Firebase user with email and password and retrieve an id token",
 	Run: func(cmd *cobra.Command, args []string) {
-		if global.Fa == nil {
-			log.Fatalf("FirebaseAuth instance is not initialized. Please run the init or load command first.")
+		configFilePath, configFile, err := internal.EnsureConfigDirAndFile()
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		defer configFile.Close()
+
+		cfg, err := internal.ReadConfig(configFile)
+		if err != nil {
+			log.Fatalf(err.Error())
 		}
 
-		_, err := global.Fa.AuthenticateUser(userEmail, userPassword)
+		settingsSection, ok := cfg["settings"].(map[string]interface{})
+		if !ok {
+			log.Fatalf("no settings section found in %s", configFilePath)
+		}
+
+		currentApp, ok := settingsSection["current-app"].(string)
+		if !ok || currentApp == "" {
+			log.Fatalf("no current app set in %s", configFilePath)
+		}
+
+		appConfig, ok := cfg[currentApp].(map[string]interface{})
+		if !ok {
+			log.Fatalf("no configuration found for app %s", currentApp)
+		}
+
+		apiKey, apiKeyOk := appConfig["api_key"].(string)
+		saKeyPath, saKeyPathOk := appConfig["sa_key_path"].(string)
+		if !apiKeyOk || !saKeyPathOk {
+			log.Fatalf("invalid API key or service account key path for app %s", currentApp)
+		}
+
+		global.Fa = &fb.FirebaseAuth{}
+		if err := global.Fa.Initialize(saKeyPath); err != nil {
+			log.Fatalf("failed to initialize Firebase: %v", err)
+		}
+		global.Fa.SetAPIKey(apiKey)
+
+		idToken, err := global.Fa.AuthenticateUser(userEmail, userPassword)
 		if err != nil {
-			log.Fatalf("Failed to sign in user: %v", err)
+			log.Fatalf("failed to authenticate user: %v", err)
 		}
 
 		fmt.Printf("ID Token retrieved. Use `id-token` command to show the token.\n")
+		fmt.Println(idToken)
 	},
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		if userEmail == "" {
